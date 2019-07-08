108a4235fb79e997d14e373b783330983683f63d
rileyjmurray <inventor1488@gmail.com>
2019-07-08 15:04:09 +0200

    added version of ex2 with equality constraint

--- examples/MCW2019/ex2.py
+++ examples/MCW2019/ex2.py
@@ -7,7 +7,7 @@ A = standard_sig_monomials(n)
 f = 1e4 * sum(A)
 main_gts = [
     1e4 + 1e-2 * A[2] / A[0] - 7.0711 / A[0],
-    1e4 * (A[2] + A[0]) + 8.54e-3 * (70.7012 * A[2] * (A[0] + A[2])) / A[0]
+    1e4 * (A[2] + A[0]) + 8.54e-3 * 70.7012 * A[2] / A[0]
         - 6.0385e-1 * (A[0] + A[2]) * (1.0 / A[0] + 1.0 / A[1])
 ]
 bounds = [
@@ -24,8 +24,8 @@ prim = sig_constrained_primal(f, main_gts, [], 0, 1, 0, X)
 dual = sig_constrained_dual(f, main_gts, [], 0, 1, 0, X)
 
 # Solve SAGE relaxations, and print the resulting objective values.
-prim.solve()
-dual.solve()
+prim.solve(verbose=False)
+dual.solve(verbose=False)
 print('\n')
 print(prim.value)
 print(dual.value)
@@ -33,3 +33,34 @@ print(dual.value)
 # Solution recovery
 solns = sig_solrec(dual)
 print(f(solns[0]))
+
+#
+#   It was possible to produce a tight bound on the equality-constrained problem all along.
+#
+
+x = standard_sig_monomials(4)
+A = x[:3]
+P = x[3]
+f = 1e4 * sum(A)
+main_gts = [
+    1e4 + 1e-2 * A[2] / A[0] - 7.0711 / A[0],
+    1e4  + 8.54e-3 * P/ A[0]
+        - 6.0385e-1 * (A[0] + A[2]) * (1.0 / A[0] + 1.0 / A[1])
+]
+bounds = [
+    1e4 - 1e4 * A[0], 1e4 * A[0] - 1e-4,
+    1e4 - 1e4 * A[1], 1e4 * A[1] - 7.0711,
+    1e4 - 1e4 * A[2], 1e4 * A[2] - 1e-4,
+    1e4 - 1e4 * P, 1e4 * P - 1e-4
+]
+gts = main_gts + bounds
+eqs = [70.7107 / A[0] + P / A[0] - P / A[2]]
+X = conditional_sage_data(f, bounds, [])
+prim = sig_constrained_primal(f, main_gts, eqs, 0, 1, 0, X)
+dual = sig_constrained_dual(f, main_gts, eqs, 0, 1, 0, X)
+# Solve SAGE relaxations, and print the resulting objective values.
+prim.solve(verbose=False)
+dual.solve(verbose=False)
+print('\n')
+print(prim.value)
+print(dual.value)
