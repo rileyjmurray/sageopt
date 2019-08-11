741d3c61a0b8760d0d8782e8514ecb42b267e23c
rileyjmurray <inventor1488@gmail.com>
2019-08-11 13:44:51 -0700

    version metadata

--- source/conf.py
+++ source/conf.py
@@ -22,7 +22,7 @@ copyright = '2019, Riley J. Murray'
 author = 'Riley J. Murray'
 
 # The full version, including alpha/beta/rc tags
-release = '0.3.0'
+release = '0.3.3'
 
 
 # -- General configuration ---------------------------------------------------
