f95ce1e3f17a9b74e93efd0aca6c3d0c95809c84
rileyjmurray <inventor1488@gmail.com>
2019-10-05 22:39:50 +0200

    prepare to remove avoid_slacks, and the various disjoint_diag_cone aspects of reformulators.py

--- sageopt/tests/test_coniclifts/test_solvers.py
+++ sageopt/tests/test_coniclifts/test_solvers.py
@@ -21,34 +21,38 @@ from sageopt.coniclifts.problems.problem import Problem
 
 class TestSolvers(unittest.TestCase):
 
+    @unittest.skipUnless(cl.Mosek.is_installed(), 'This is a MOSEK-specific test.')
     def test_mosek_avoid_slacks_1(self):
+        # This test is failing, and it's failing because the different calls
+        # to .solve() actually result
         x = cl.Variable(shape=(2,), name='x')
         y = cl.Variable(shape=(2,), name='y')
         z = cl.Variable(shape=(4,), name='z')
-        re = cl.relent(2 * x + 1, np.exp(1) * y)
+        re = cl.relent(2 * x + 1, y)
         t = cl.Variable()
         obj = cl.vector2norm(z)
         con = [re <= 10, 3 <= x, x <= 5, obj <= t, cl.hstack((x, y)) == z]
         prob = Problem(cl.MIN, t, con)
-        prob.solve(solver='MOSEK',
-                   cache_apply_data=True,
-                   compilation_options={'avoid_slacks': True})
+        r0 = prob.solve(solver='MOSEK',
+                        cache_apply_data=True,
+                        compilation_options={'avoid_slacks': True})
         ad0 = prob.solver_apply_data['MOSEK']
-        x0 = x.value
-        prob.solve(solver='MOSEK',
-                   cache_apply_data=True,
-                   compilation_options={'avoid_slacks': False})
+        z0 = z.value
+        r1 = prob.solve(solver='MOSEK',
+                        cache_apply_data=True,
+                        compilation_options={'avoid_slacks': False})
         ad1 = prob.solver_apply_data['MOSEK']
-        x1 = x.value
-        assert np.allclose(x0, x1)
+        z1 = z.value
+        assert np.allclose(z0, z1)
         assert ad0[0]['A'].shape[0] < ad1[0]['A'].shape[0]
         pass
 
+    @unittest.skipUnless(cl.Mosek.is_installed(), 'This is a MOSEK-specific test.')
     def test_mosek_avoid_slacks_2(self):
         x = cl.Variable(shape=(2,), name='x')
         y = cl.Variable(shape=(2,), name='y')
         z = cl.Variable(shape=(4,), name='z')
-        re = cl.relent(x, [y[0], x[1] + 3 * y[1]])
+        re = cl.relent(x + 1, [np.exp(1) * y[0], x[1] + 3 * y[1]])
         t = cl.Variable()
         obj = cl.vector2norm(z)
         con = [re <= 10, 3 <= x, x <= 5, obj <= t, cl.hstack((x, y)) == z]
@@ -57,13 +61,13 @@ class TestSolvers(unittest.TestCase):
                    cache_apply_data=True,
                    compilation_options={'avoid_slacks': True})
         ad0 = prob.solver_apply_data['MOSEK']
-        x0 = x.value
+        z0 = z.value
         prob.solve(solver='MOSEK',
                    cache_apply_data=True,
                    compilation_options={'avoid_slacks': False})
         ad1 = prob.solver_apply_data['MOSEK']
-        x1 = x.value
-        assert np.allclose(x0, x1)
+        z1 = z.value
+        assert np.allclose(z0, z1)
         assert ad0[0]['A'].shape[0] < ad1[0]['A'].shape[0]
         pass
 
