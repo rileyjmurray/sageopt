b662545dd5d85396a588203ff15096e66f8bf671
rileyjmurray <inventor1488@gmail.com>
2019-10-05 21:21:44 +0200

    added a test

--- sageopt/tests/test_coniclifts/test_solvers.py
+++ sageopt/tests/test_coniclifts/test_solvers.py
@@ -21,11 +21,34 @@ from sageopt.coniclifts.problems.problem import Problem
 
 class TestSolvers(unittest.TestCase):
 
-    def test_mosek_avoid_slacks(self):
+    def test_mosek_avoid_slacks_1(self):
         x = cl.Variable(shape=(2,), name='x')
         y = cl.Variable(shape=(2,), name='y')
         z = cl.Variable(shape=(4,), name='z')
         re = cl.relent(2 * x + 1, np.exp(1) * y)
+        t = cl.Variable()
+        obj = cl.vector2norm(z)
+        con = [re <= 10, 3 <= x, x <= 5, obj <= t, cl.hstack((x, y)) == z]
+        prob = Problem(cl.MIN, t, con)
+        prob.solve(solver='MOSEK',
+                   cache_apply_data=True,
+                   compilation_options={'avoid_slacks': True})
+        ad0 = prob.solver_apply_data['MOSEK']
+        x0 = x.value
+        prob.solve(solver='MOSEK',
+                   cache_apply_data=True,
+                   compilation_options={'avoid_slacks': False})
+        ad1 = prob.solver_apply_data['MOSEK']
+        x1 = x.value
+        assert np.allclose(x0, x1)
+        assert ad0[0]['A'].shape[0] < ad1[0]['A'].shape[0]
+        pass
+
+    def test_mosek_avoid_slacks_2(self):
+        x = cl.Variable(shape=(2,), name='x')
+        y = cl.Variable(shape=(2,), name='y')
+        z = cl.Variable(shape=(4,), name='z')
+        re = cl.relent(x, [y[0], x[1] + 3 * y[1]])
         t = cl.Variable()
         obj = cl.vector2norm(z)
         con = [re <= 10, 3 <= x, x <= 5, obj <= t, cl.hstack((x, y)) == z]
