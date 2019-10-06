506d4767d0ce49fdc3141516ad3b8996a35e942a
rileyjmurray <inventor1488@gmail.com>
2019-10-06 02:19:09 +0200

    removed some static functions from cone.py, which were no longer used

--- sageopt/coniclifts/cones.py
+++ sageopt/coniclifts/cones.py
@@ -29,29 +29,3 @@ class Cone(object):
             return self.type == other.type and self.len == other.len and self.annotations == other.annotations
         else:
             return False
-
-    @staticmethod
-    def annotate_cone_positions(K):
-        running_idx = 0
-        for i, co in enumerate(K):
-            co.annotations['ss'] = (running_idx, running_idx + co.len)
-            co.annotations['position'] = i
-            running_idx += co.len
-        pass
-
-    @staticmethod
-    def annotate_cone_scopes(A, K):
-        # assumes entries of K have already been annotated with position data.
-        for co in K:
-            co_start, co_stop = co.annotations['ss']
-            if 'A' in co.annotations:
-                curr_A = co.annotations['A']
-            else:
-                curr_A = A[co_start:co_stop, :]
-            co.annotations['scope'] = {i for i in curr_A.nonzero()[1]}
-        pass
-
-
-
-
-
