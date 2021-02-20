caf47557016895199f38cac313a62fbef2afe81c
anish <asenapat@caltech.edu>
2021-02-19 19:40:18 -0800

    Formatted/fixed dosctrings

--- sageopt/coniclifts/constraints/set_membership/pow_cone.py
+++ sageopt/coniclifts/constraints/set_membership/pow_cone.py
@@ -7,42 +7,92 @@ from sageopt.coniclifts.problems.problem import Problem
 
 
 class PowCone(SetMembership):
+    """
+    A power cone constraint is defined with an n-dimensional vector
+    :math:'\alpha' which is elementwise positive and
+    sums to 1. The cone can be summarized by writing that
+    :math:'(w, z) \\in C_{\\mathrm{power}}' if:
+
+    .. math::
+        \\prod_{i=0}^n w_i^\\alpha_i > \\| z \\|
+
+    where :math:'w' is an n dimensional vector and :math:'z' is a scalar.
+
+    Parameters
+    ----------
+
+    w : Expression
+
+        An (n+1)-dimensional vector subject to this Power Cone constraints.
+        One element of this vector is z which is
+        determined by the elements of
+
+    lamb: ndarray
+        An (n+1)-dimensional array with 1 negative entry and n positive entries
+        whose sum is 0. The n positive entries determine the elements of
+        :math:'\\alpha' while the negative entry of lamb determines the index of
+        z within the w parameter.
+
+
+    Attributes
+    ----------
+    id: int
+        The id of the Power Cone constraint object
+
+    w : Expression
+        The inputted (n+1)-dimensional vector inputted
+        into the Power Cone constructor
+
+    lamb : ndarray
+        The inputted (n+1)-dimensional array inputted into the
+        Power Cone constructor that sums to 0
+
+    w_low : Expression
+        An n-dimensional expression used to describe w in the Power Cone
+
+    z_low : Expression
+        A scalar expression used to describe z in the Power Cone
+
+    alpha : ndarray
+        The n-dimensional vector :math:'\\alpha' that sums to 1
+        that defines the power Cone constraint
+
+    Notes
+    -----
+
+    The constructor can raise a RuntimeError if the constraint is deemed infeasible.
+    """
     _CONSTRAINT_ID_ = 0
 
-    """
-        Creates an instance of a Power Cone whose constraints are described in cone_standards.txt
-        
-         In this constructor, w is an (n+1)-vector and lamb is an (n+1) vector with exactly one negative 
-         component.  lamb in the input corresponds to alpha in the description of a power cone.
-         
-         The negative number in lamb corresponds to the index of w that is z in the description of a power cone.
-    """
     def __init__(self, w, lamb):
+
         self.id = PowCone._CONSTRAINT_ID_
         PowCone._CONSTRAINT_ID_ += 1
 
         # Finds indices of all positive elements of lambs (alpha)
         pos_idxs = lamb > 0
 
-        #Error if lamb and w are not same size
+        # Error if lamb and w are not same size
         if not w.size == lamb.size:
-            raise RuntimeError('Incompatible dimensions for w (' + str(w.size) + ') and alpha (' + str(lamb.size) + ')')
+            msg = 'Incompatible dimensions for w (%s) and alpha (%s)' % (w.size, lamb.size)
+            raise ValueError(msg)
 
         # Error if alpha has no negative number
-        if np.sum(pos_idxs) == np.size(lamb):
-            raise ValueError('No negative number in lamb array')
+        if np.all(pos_idxs):
+            msg = 'No negative number in inputted lamb array'
+            raise ValueError(msg)
 
-        negative = lamb[lamb < 0]
-        posotive = np.sum(lamb[pos_idxs])
-        if np.sum(lamb[pos_idxs]) + lamb[lamb < 0] != 0:
-            raise ValueError('lamb does not have sum of 0')
+        neg_idxs = lamb < 0
+        if np.sum(lamb) != 0:
+            msg = 'lamb does not have sum of 0'
+            raise ValueError(msg)
+
         # Get positive values and normalize
-        alpha_low = lamb[pos_idxs]/np.abs(lamb[lamb < 0])
-
+        alpha_low = lamb[pos_idxs]/np.abs(lamb[neg_idxs])
 
         # Negative number corresponds to z in power cone and rest of numpy array is w
         w_low = w[pos_idxs]
-        z_low = w[lamb < 0]
+        z_low = w[neg_idxs]
 
         if not isinstance(w_low, Expression):
             w_low = Expression(w_low)
@@ -56,16 +106,19 @@ class PowCone(SetMembership):
         self.alpha = alpha_low
         pass
 
-    """
-        Returns a list of all the variables asssociated with this power cone
-    """
     def variables(self):
+        """
+        Return a list of all Variable objects appearing in this Power Cone constraint object
 
+        Returns
+        -------
+        - A list of all the variables involved in the Power Cone constraint of this object
+        """
         var_ids = set()
         var_list = []
 
         # Grab non duplicate elements from w
-        for se in self.w_low.ravel():
+        for se in self.w_low.flat():
             for sv in se.scalar_variables():
                 if id(sv.parent) not in var_ids:
                     var_ids.add(id(sv.parent))
@@ -79,15 +132,21 @@ class PowCone(SetMembership):
 
         return var_list
 
-    """
-        Returns the sparse form representation of the Power Cone problem (Ax = b)
-        
-        The returned elements follow the conic form method in constraint.py. Additionally, the cones with K contains an 
-        annotation that store the alpha parameter of this power cone object.
-    """
     def conic_form(self):
+        """
+        Returns a sparse matrix representation of the Power cone object. It represents of the object as
+        :math:'Ax+b \\in K' where K is a cone object
+
+        Returns
+        -------
+        A_vals - list (of floats)
+        A_rows - numpy 1darray (of integers)
+        A_cols - list (of integers)
+        b - numpy 1darray (of floats)
+        K - list (of coniclifts Cone objects)
+        """
         A_rows, A_cols, A_vals = [], [], []
-        K = [Cone('pow', self.w.size, annotations={'weights': np.array(self.alpha).tolist()})]
+        K = [Cone('pow', self.w.size, annotations={'weights': self.alpha.tolist()})]
         b = np.zeros(shape=(self.w.size,))
 
         # Loop through w and add every variable
@@ -124,6 +183,17 @@ class PowCone(SetMembership):
 
     @staticmethod
     def project(item, alpha):
+        """
+        Calculates the shortest distance (the projection) of a vector to a cone parametrized by :math:'\\alpha'
+        Parameters
+        ----------
+        item - the point we are projecting
+        alpha - the :math:'\\alpha' parameter for the Cone that we are projecting to
+
+        Returns
+        -------
+        The distance of the projection to the Cone
+        """
         from sageopt.coniclifts import MIN as CL_MIN
 
         item = Expression(item).ravel()
@@ -140,10 +210,20 @@ class PowCone(SetMembership):
 
         return prob.value
 
-    """
-    Returns the violation of the current values of this object 
-    """
-    def violation(self, rough = False):
+    def violation(self, rough=False):
+        """
+        Returns the violation of stored w and z to being in the Power cone parametrized by alpha. If rough = True, then
+        measures the violation based off the maximum violation of the condition of the Power Cone. If not, it is based
+        off the projection distance between the point and Cone.
+
+        Parameters
+        ----------
+        rough - whether to use a rough approximation of the violation
+
+        Returns
+        -------
+        The value of the violation of this object's w and z
+        """
         if rough:
             return np.max([np.abs(self.z_low.value) - np.prod(np.power(self.w_low.value, self.alpha)), 0])
         else:
